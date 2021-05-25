b9a95f49d17e49d5b5f65eaa01c5bdae81969ad7
unknown <jorammanoah1@gmail.com>
2021-05-24 22:21:10 -0700

    Added tailwind

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function About() {
+    return (
+        <div>
+            <Head>
+                <title>Venice' | About Us </title>
+                
+            </Head>
+            Its about us
+        </div>
+    )
+}
