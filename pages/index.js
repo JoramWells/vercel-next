e16a3eaa2eed1b9140f7615012570536601bba9c
unknown <jorammanoah1@gmail.com>
2021-05-24 23:45:49 -0700

    Added heroicons

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from 'next/image'
-import { ViewGridIcon } from "@heroicons/react/solid";
+import { MicrophoneIcon, SearchIcon, ViewGridIcon } from "@heroicons/react/solid";
 
 export default function Home() {
   return (
@@ -31,12 +31,21 @@ export default function Home() {
         </div>
       </header>
       {/* Body */}
-      <form>
+      <form className="flex flex-col items-center mt-24">
         <Image
         src="https://www.thehawk.in/hawkcontent/servlet/RDESController?command=rdm.Picture&sessionId=RDWEBD9ZXBMHEVOZY44KG0ZIGGJKEI9YBQY0U&app=rdes&partner=thehawk&type=7&uid=7fqeTRRcJ4ftYOyw41rbQiUSYivnjIvrF5837497"
         height={100}
-        width={300}
+        width={190}
          />
+         <div className="flex w-full mt-5 hover:shadow-lg rounded-full border-gray-200 px-5 py-3 items-center max-w-md sm:max-w-xl lg:max-w-2xl" >
+           <SearchIcon className="h-5 mr-3 text-gray-500" />
+           <input type="text" className="flex-grow focus:outline-none" />
+           <MicrophoneIcon className="h-5" />
+         </div>
+         <div>
+           <button className="btn">Google Search</button>
+           <button className="btn">I'm feeling lucky</button>
+         </div>
       </form>
       {/* Footer */}
     </div>
